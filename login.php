315c240a28d502834b62df54727fc563297e0f20
APARIACURI <pariacuri012@gmail.com>
2026-04-25 13:03:10 -0500

    modifique el index y ademas agregue el login php

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+if ($usuario == "admin" && $password == "1234") {
+    echo "Bienvenido, admin!";
+} else {
+    echo "Usuario o contraseña incorrectos.";
+}
+?>
